982766f4a15c44ab015a5734e6d73ef6f3870f8d
SOvsyankin <sovsyankin@beeline.kz>
2021-01-08 21:45:41 +0600

    Accessing Request Path and HTTP Headers

--- src/main/java/com/sostud/photoapp/api/gateway/MyPreFilter.java
+++ src/main/java/com/sostud/photoapp/api/gateway/MyPreFilter.java
@@ -1,9 +1,11 @@
 package com.sostud.photoapp.api.gateway;
 
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -16,6 +18,14 @@ public class MyPreFilter implements GlobalFilter {
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
     logger.info("My first preFilter is executed");
+    final String requestPath = exchange.getRequest().getPath().toString();
+    logger.info("Request path {}", requestPath);
+    final HttpHeaders headers = exchange.getRequest().getHeaders();
+    final Set<String> headerNames = headers.keySet();
+    headerNames.forEach(headerName -> {
+      final String headerValue = headers.getFirst(headerName);
+      logger.info("{}: {}", headerName, headerValue);
+    });
     return chain.filter(exchange);
   }
 }
