4c947a33e89b9388e2ff0e79427cb523c81ad078
SOvsyankin <sovsyankin@beeline.kz>
2021-01-11 09:33:11 +0600

    Ordering Filters

--- src/main/java/com/sostud/photoapp/api/gateway/GlobalFiltersConfig.java
+++ src/main/java/com/sostud/photoapp/api/gateway/GlobalFiltersConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -13,29 +14,32 @@ public class GlobalFiltersConfig {
   final Logger logger = LoggerFactory.getLogger(GlobalFiltersConfig.class);
 
   @Bean
+  @Order(1)
   public GlobalFilter secondPreFilter() {
     return ((exchange, chain) -> {
       logger.info("My second global pre-filter is executed...");
-      return chain.filter(exchange).then(Mono.fromRunnable(()
-          -> logger.info("My second global post-filter is executed")));
-    });
-  }
-
-  @Bean
-  public GlobalFilter thirdPreFilter() {
-    return ((exchange, chain) -> {
-      logger.info("My third global pre-filter is executed...");
       return chain.filter(exchange).then(Mono.fromRunnable(()
           -> logger.info("My third global post-filter is executed")));
     });
   }
 
   @Bean
+  @Order(2)
+  public GlobalFilter thirdPreFilter() {
+    return ((exchange, chain) -> {
+      logger.info("My third global pre-filter is executed...");
+      return chain.filter(exchange).then(Mono.fromRunnable(()
+          -> logger.info("My second global post-filter is executed")));
+    });
+  }
+
+  @Bean
+  @Order(3)
   public GlobalFilter fourthPreFilter() {
     return ((exchange, chain) -> {
       logger.info("My fourth global pre-filter is executed...");
       return chain.filter(exchange).then(Mono.fromRunnable(()
-          -> logger.info("My fourth global post-filter is executed")));
+          -> logger.info("My first global post-filter is executed")));
     });
   }
 
--- src/main/java/com/sostud/photoapp/api/gateway/MyPreFilter.java
+++ src/main/java/com/sostud/photoapp/api/gateway/MyPreFilter.java
@@ -5,13 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class MyPreFilter implements GlobalFilter {
+public class MyPreFilter implements GlobalFilter, Ordered {
 
   final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
 
@@ -27,5 +28,10 @@ public class MyPreFilter implements GlobalFilter {
       logger.info("{}: {}", headerName, headerValue);
     });
     return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return 0;
   }
 }
